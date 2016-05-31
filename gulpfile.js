727adf86d51081ccbde270a52780eb6f0950298d
armandobasha <armando.basha@yahoo.com>
2016-05-31 17:33:02 +0200

    - initialized npm on this directory by creating package.json file - installed bower, gulp, gulp-clean, gulp-concat, less, gulp-uglify, gulp-less as node modules to manage assets - created web-src folder that will contain unprocessed assets like images, js files, and less files (maybe add fonts). - installed bootstrap with bower  - created app.less file and imported bootstrap.css

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+/*---------- gulpfile.js ------------*/
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    clean = require('gulp-clean'),
+    concatJs = require('gulp-concat'),
+    minifyJs = require('gulp-uglify');
+
+var bower_components_path = 'web/assets/vendor',
+    assets_path = 'web/assets';
+
+gulp.task('less', function() {
+    return gulp.src(['web-src/less/*.less'])
+        .pipe(less({compress: true}))
+        .pipe(gulp.dest(assets_path + '/css/'));
+});
+
+gulp.task('images', function () {
+    return gulp.src([
+            'web-src/images/*'
+        ])
+        .pipe(gulp.dest(assets_path + '/images/'))
+});
+
+gulp.task('fonts', function () {
+    return gulp.src([bower_components_path + '/bootstrap/fonts/*'])
+        .pipe(gulp.dest('web/fonts/'))
+});
+
+gulp.task('lib-js', function() {
+    return gulp.src([
+            bower_components_path + '/jquery/dist/jquery.js',
+            bower_components_path + '/bootstrap/dist/js/bootstrap.js'
+        ])
+        .pipe(concatJs('app.js'))
+        .pipe(minifyJs())
+        .pipe(gulp.dest(assets_path + '/js/'));
+});
+
+gulp.task('pages-js', function() {
+    return gulp.src([
+            'web-src/js/*.js'
+        ])
+        .pipe(minifyJs())
+        .pipe(gulp.dest('web/js/'));
+});
+
+gulp.task('clean', function () {
+    return gulp.src([assets_path + '/css/*', assets_path + '/js/*', assets_path + '/images/*', assets_path + '/fonts/*'])
+        .pipe(clean());
+});
+
+gulp.task('default', ['clean'], function () {
+    var tasks = ['images', 'fonts', 'less', 'lib-js', 'pages-js'];
+    tasks.forEach(function (val) {
+        gulp.start(val);
+    });
+});
+
+gulp.task('watch', function () {
+    var less = gulp.watch('web-src/less/*.less', ['less']),
+        js = gulp.watch('web-src/js/*.js', ['pages-js']);
+});
